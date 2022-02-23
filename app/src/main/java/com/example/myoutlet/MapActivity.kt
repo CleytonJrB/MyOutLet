2dbeaa657b54bb58cff2eec834847f147b6c7d09
cleytonjrb <cleytonjrb@outlook.com>
2022-02-23 12:46:18 -0300

    Error resolved

--- app/src/main/java/com/example/myoutlet/MapActivity.kt
+++ app/src/main/java/com/example/myoutlet/MapActivity.kt
@@ -2,7 +2,6 @@ package com.example.myoutlet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myoutlet.model.Maps
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
@@ -12,17 +11,16 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapActivity : AppCompatActivity() {
 
-  private val places : ArrayList<Place> ?= null
+  private val places = arrayListOf(
 
+  Place("C${"&"}A","Av. República do Líbano, 251 - Pina, Recife - PE",LatLng(-8.0864409,-34.896674)),
+        Place("Renner","R. Manoel Hónorato da Costa, 555 - Vila da Fabrica, Camaragibe - PE",LatLng(-8.0163327,-34.9775849))
+
+  )
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.pg_map)
-
-    val teste1 = places
-
-
-    val mapData = intent.extras
 
 
     val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
@@ -32,7 +30,7 @@ class MapActivity : AppCompatActivity() {
       googleMap.setOnMapLoadedCallback {
         val bounds = LatLngBounds.builder()
 
-        places?.forEach { place ->
+        places.forEach { place ->
           bounds.include(place.latLng)
         }
         googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 100))
@@ -40,7 +38,7 @@ class MapActivity : AppCompatActivity() {
     }
   }
   private fun addMarkers(googleMap: GoogleMap){
-    places?.forEach{ place ->
+    places.forEach{ place ->
       googleMap.addMarker(
         MarkerOptions()
           .title(place.name)
